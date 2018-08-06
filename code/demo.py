c45e197b27c8e1e7adb95cc3ad186e52b9b0a817
JaeZheng <846265697@qq.com>
2018-08-06 16:25:42 +0800

    enable FM & AFM to solve CTR problem.

---
+++ code/demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : JaeZheng
+# @Time    : 2018/8/6 10:15
+# @File    : demo.py
+
+from LoadCTRData import LoadCTRData as DATA
+
+data = DATA("../data/", "ctr")
